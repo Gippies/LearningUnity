991f28ec1731a5d416c654358b279a1c2e564e3e
Joshua Patterson <jleegippies@gmail.com>
2019-10-16 21:59:38 -0500

    Added a cube that chases the player cube (and runs away if too close). TODO: Need to fix movement when object flips

---
+++ Assets/Scripts/Chaser.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chaser : MonoBehaviour
+{
+    public Transform targetTransform;
+    private float speed = 7.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 displacementFromTarget = targetTransform.position - transform.position;
+        Vector3 directionToTarget = displacementFromTarget.normalized;
+        Vector3 velocity = directionToTarget * speed;
+
+        Vector3 displacementAwayFromTarget = transform.position - targetTransform.position;
+        Vector3 directionAwayFromTarget = displacementAwayFromTarget.normalized;
+        Vector3 moveAwayVelocity = directionAwayFromTarget * speed;
+
+        float distanceToTarget = displacementFromTarget.magnitude;
+        
+        if (distanceToTarget > 3.0f) {
+            transform.Translate(velocity * Time.deltaTime);
+        }
+        else if (distanceToTarget < 2.0f) {
+            transform.Translate(moveAwayVelocity * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerCubeBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerCubeBehaviour : MonoBehaviour
+{
+    private float speed = 10.0f;
+    private float jumpSpeed = 8.0f;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        UpdatePosition();
+    }
+
+    private float GetDistanceBetweenTwoPoints(float x1, float y1, float x2, float y2) {
+        return Mathf.Sqrt(Mathf.Pow(x2 - x1, 2.0f) + Mathf.Pow(y2 - y1, 2.0f));
+    }
+
+    private void UpdatePosition()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 direction = input.normalized;
+        Vector3 velocity = direction * speed;
+
+        transform.Translate(velocity * Time.deltaTime);
+
+         if (Input.GetKeyDown("space")) {
+             rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+         }
+    }
+}
